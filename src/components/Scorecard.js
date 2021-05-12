01603f822f66ac0de223f7a9e881cea3bf9cb4d1
Jim Burch <jrburch1@gmail.com>
2021-05-11 23:39:46 -0700

    higher order components for frames

--- src/components/Scorecard.js
+++ src/components/Scorecard.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function Scorecard() {
+	function renderScorecard() {
+		const framesArray = [];
+		for (let i = 1; i < 10; i++) {
+			framesArray.push(i);
+		}
+		return framesArray.map((index, frame) => (
+			<div className="frame" key={index} value={frame}>
+				y<div className="frame-corner">x</div>
+			</div>
+		));
+	}
+
+	const frames = renderScorecard();
+
 	return (
 		<div className="scorecard">
 			<h1>Scorecard</h1>
+			<div className="frames-container">
+				{frames}
+				<div className="frame" value={10}>
+					y<div className="frame-corner">x</div>
+					<div className="frame-corner">x</div>
+				</div>
+			</div>
 		</div>
 	);
 }
