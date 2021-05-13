4c4806072a9d7d7efab65f3803a0b0a150caff41
Jim Burch <jrburch1@gmail.com>
2021-05-13 00:20:15 -0700

    added functions to handle strikes and spares

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,6 +4,7 @@ function Input({ boxScore, setBoxScore }) {
 	const [frame, setFrame] = useState(1);
 	const [turn, setTurn] = useState(1);
 	const [pins, setPins] = useState(10);
+
 	function renderPins(pins) {
 		const pinArray = [];
 		for (let i = 0; i <= pins; i++) {
@@ -14,6 +15,91 @@ function Input({ boxScore, setBoxScore }) {
 				{pin}
 			</button>
 		));
+	}
+
+	function calculateStrike(currentFrame) {
+		let frame = boxScore[currentFrame];
+		if (boxScore[currentFrame - 1]) {
+			frame[2] =
+				boxScore[currentFrame - 1][2] +
+				frame[3] +
+				boxScore[currentFrame + 1][3] +
+				boxScore[currentFrame + 2][3];
+		} else {
+			frame[2] =
+				frame[3] +
+				boxScore[currentFrame + 1][3] +
+				boxScore[currentFrame + 2][3];
+		}
+	}
+
+	function calculateSpare(currentFrame) {
+		let frame = boxScore[currentFrame];
+		if (boxScore[currentFrame - 1]) {
+			frame[2] =
+				boxScore[currentFrame - 1][2] +
+				frame[3] +
+				boxScore[currentFrame + 1][3];
+		} else {
+			frame[2] = frame[3] + boxScore[currentFrame + 1][3];
+		}
+	}
+
+	function calculateScore(currentFrame) {
+		let frame = boxScore[currentFrame];
+		if (boxScore[currentFrame - 1]) {
+			frame[2] = boxScore[currentFrame - 1][2] + frame[3];
+		} else {
+			frame[2] = frame[3];
+		}
+		console.log(frame);
+	}
+
+	function scoreLoop(currentFrame) {
+		for (let i = 1; i <= currentFrame; i++) {
+			if (boxScore[i][1] === 'X' && boxScore[i + 2][3]) {
+				calculateStrike(i);
+				console.log('calc strike!');
+			} else if (boxScore[i][1] === '/' && boxScore[i + 1][3]) {
+				calculateSpare(i);
+				console.log('calc spare!');
+			} else if (
+				boxScore[i][1] &&
+				boxScore[i][1] !== 'X' &&
+				boxScore[i][1] !== '/'
+			) {
+				calculateScore(i);
+				console.log('calc score!');
+			}
+		}
+	}
+
+	function updateScorecard(currentFrame, currentTurn, currentScore) {
+		let frame = boxScore[currentFrame];
+
+		if (currentTurn === 1 && currentScore === '10') {
+			frame[currentTurn] = 'X';
+			frame[3] = 10;
+			setBoxScore({ ...boxScore, currentFrame: frame });
+			console.log('strike!');
+		} else if (
+			currentTurn === 2 &&
+			Number(frame[0]) + Number(currentScore) === 10
+		) {
+			frame[currentTurn - 1] = '/';
+			frame[3] = 10;
+			setBoxScore({ ...boxScore, currentFrame: frame });
+			console.log('spare!');
+		} else {
+			frame[currentTurn - 1] = currentScore;
+			if (currentTurn === 2) {
+				frame[3] = Number(frame[0]) + Number(frame[1]);
+			}
+			setBoxScore({ ...boxScore, currentFrame: frame });
+			console.log('score!');
+		}
+
+		scoreLoop(currentFrame);
 	}
 
 	function handleClick(e) {
@@ -32,33 +118,6 @@ function Input({ boxScore, setBoxScore }) {
 	}
 
 	const pinChoices = renderPins(pins);
-
-	function updateScorecard(currentFrame, currentTurn, currentScore) {
-		let frame = boxScore[currentFrame];
-
-		if (currentTurn === 1 && currentScore === '10') {
-			frame[currentTurn] = 'X';
-			setBoxScore({ ...boxScore, currentFrame: frame });
-			console.log('strike!');
-		} else if (
-			currentTurn === 2 &&
-			Number(frame[0]) + Number(currentScore) === 10
-		) {
-			frame[currentTurn - 1] = '/';
-			setBoxScore({ ...boxScore, currentFrame: frame });
-			console.log('spare!');
-		} else {
-			frame[currentTurn - 1] = currentScore;
-			setBoxScore({ ...boxScore, currentFrame: frame });
-			console.log('score!');
-		}
-	}
-
-	function calculateScores() {
-		for (let frame in boxScore) {
-			// figure out how to do this shit
-		}
-	}
 
 	return (
 		<div className="input">
--- src/components/Scorecard.js
+++ src/components/Scorecard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Scorecard({ boxScore }) {
+function Scorecard({ score, setScore, boxScore, setBoxScore }) {
 	function renderScorecard() {
 		const frameArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 		return frameArray.map((frame, index) => (
-			<div className="frame" key={index} value={frame}>
+			<div className="frame" key={index}>
 				<div className="turn-one">{boxScore[frame][0]}</div>
 				<div className="turn-two">{boxScore[frame][1]}</div>
 				<div className="subtotal">{boxScore[frame][2]}</div>
@@ -22,7 +22,7 @@ function Scorecard({ boxScore }) {
 					<div className="turn-one">{boxScore[10][0]}</div>
 					<div className="turn-two">{boxScore[10][1]}</div>
 					<div className="turn-three">{boxScore[10][2]}</div>
-					<div className="subtotal">{boxScore[10][3]}</div>
+					<div className="subtotal"></div>
 				</div>
 			</div>
 		</div>
