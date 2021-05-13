2ff6535e6989e1689e20528767b008164c91e0fc
Jim Burch <jim@Jims-MacBook-Pro.local>
2021-05-13 14:57:19 -0700

    changed boxscore to accomdate edge cases

--- src/App.js
+++ src/App.js
@@ -6,23 +6,30 @@ import './App.css';
 function App() {
 	const [score, setScore] = useState(0);
 	const [boxScore, setBoxScore] = useState({
-		// frame: [turn 1, turn 2, (turn 3), subtotal, frame total]
-		1: ['', '', null, null],
-		2: ['', '', null, null],
-		3: ['', '', null, null],
-		4: ['', '', null, null],
-		5: ['', '', null, null],
-		6: ['', '', null, null],
-		7: ['', '', null, null],
-		8: ['', '', null, null],
-		9: ['', '', null, null],
-		10: ['', '', '', null, null],
+		// frame: [turn 1, turn 2, (turn 3), game subtotal, frame total]
+		0: ['', '', null, null, 1],
+		1: ['', '', null, null, 0],
+		2: ['', '', null, null, 0],
+		3: ['', '', null, null, 0],
+		4: ['', '', null, null, 0],
+		5: ['', '', null, null, 0],
+		6: ['', '', null, null, 0],
+		7: ['', '', null, null, 0],
+		8: ['', '', null, null, 0],
+		9: ['', '', null, null, 0],
+		10: ['', '', null, null, 0],
+		11: ['', '', null, null, 0],
+		12: ['', '', null, null, 0],
+		13: ['', '', null, null, 0],
+		14: ['', '', null, null, 0],
+		15: ['', '', null, null, 0],
+		16: ['', '', null, null, 0],
+		// 10: ['', '', '', null, null],
 	});
 
 	return (
 		<div className="App">
 			<h1>Bowling Scorecard</h1>
-			<h3>Score: {score}</h3>
 			<Input boxScore={boxScore} setBoxScore={setBoxScore} />
 			<Scorecard
 				score={score}
