1528f779d438383f30b3cd900188fcfa05470d1f
Jim Burch <jim@Jims-MacBook-Pro.local>
2021-05-14 16:34:09 -0700

    fixed endframe bug

--- src/components/Scorecard.js
+++ src/components/Scorecard.js
@@ -5,6 +5,7 @@ function Scorecard({ boxScore, setBoxScore }) {
 		const frameArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 		return frameArray.map((frame, index) => (
 			<div className="frame" key={index}>
+				<div className="frame-number">{frame}</div>
 				<div className="frame-top-row">
 					<div className="turn-one">{boxScore[frame][0]}</div>
 					<div className="turn-two">{boxScore[frame][1]}</div>
@@ -17,7 +18,6 @@ function Scorecard({ boxScore, setBoxScore }) {
 	const frames = renderScorecard();
 
 	function renderFinalScore() {
-		console.log(boxScore);
 		// open
 		if (boxScore[11][1] && boxScore[10][3] + boxScore[11][3] < 10) {
 			return boxScore[11][2];
@@ -45,6 +45,7 @@ function Scorecard({ boxScore, setBoxScore }) {
 			<div className="frames-container">
 				{frames}
 				<div className="last-frame" value={10}>
+					<div className="frame-number">10</div>
 					<div className="last-frame-top-row">
 						<div className="last-turn-one">{boxScore[10][1]}</div>
 						<div className="last-turn-two">{boxScore[11][1]}</div>
